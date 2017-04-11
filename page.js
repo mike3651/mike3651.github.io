fcb2d129bd40936640ee88f333fb3dfdadd46727
Michael <anthony_michael_wilson@yahoo.com>
2017-04-10 18:04:18 -0700

    added a new section! :)

--- page.js
+++ page.js
@@ -1,4 +1,15 @@
 (function() {
 	window.onload = function() {				
+		updateImages();
+	}
+
+	// function the updates images of my upcoming endeavors
+	function updateImages() {		
+		for(var i = 1; i < 5; i++) {			
+			var img = document.createElement("img");
+			img.src = "project_images/mathematics/derivative_" + i + ".png";
+			img.className = "endeavor-image";			
+			$("#endeavor-updates").append(img);
+		}		
 	}
 })();
